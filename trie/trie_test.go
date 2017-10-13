34b470d0d8f8d327aed166f1838ad7d512900854
wjcloud <shnano1128@naver.com>
2017-10-14 04:00:26 +0900

    FIx conflict

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -30,10 +30,10 @@ import (
 	"testing/quick"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/EthereumVega/EVA-00D/common"
+	"github.com/EthereumVega/EVA-00D/crypto"
+	"github.com/EthereumVega/EVA-00D/ethdb"
+	"github.com/EthereumVega/EVA-00D/rlp"
 )
 
 func init() {
